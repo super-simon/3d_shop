0e5abc2dc07599846ecc1787589c108838737397
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2024-02-07 13:47:48 +0200

    Customize It button

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,5 +1,26 @@
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+const CustomButton = ({ title, type, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "white",
+      };
+    }
+  };
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default CustomButton;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -38,7 +38,12 @@ const Home = () => {
                 customization tool. <strong>Unsplash your imagination</strong>{" "}
                 and define your own style.
               </p>
-              <CustomButton />
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
